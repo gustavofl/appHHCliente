de0eb94164a4077fa59e2d5d769a79ba35b0b3a1
gustavofl <gustavofernandezluz@gmail.com>
2019-03-19 13:44:38 -0300

    Rolagem da tela OK

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import Destaques from './src/screens/destaquesMapa';
+import TelaDestaquesMapa from './src/screens/destaquesMapa';
 
 export default class App extends Component {
   render() {
-    return <Destaques />
+    return <TelaDestaquesMapa />
   }
 }
--- src/screens/destaquesMapa.js
+++ src/screens/destaquesMapa.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import colors from '../styles/colors';
-import {Platform, StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {Platform, StyleSheet, Text, View, ImageBackground, ScrollView } from 'react-native';
 
 class AreaAnuncio extends Component {
   render() {
@@ -18,19 +18,29 @@ class AreaAnuncio extends Component {
   }
 }
 
-export default class Destaques extends Component {
+class Destaques extends Component {
   render() {
     return (
-      <ImageBackground
-        style={{flex:1, alignSelf: 'stretch', height: undefined, width: undefined, left:0}}
-        source={require('../img/mapa_garanhuns.png')}
-      >
+      <ScrollView>
         <AreaAnuncio titulo='Tapioca do Matuto' texto1='R. Dantas Barreto, 78-A' texto2='Abre às 17:00' cor={colors.cinzaEscuro} />
         <AreaAnuncio titulo='Coração sertanejo' texto1='R. Ari Barroso, 1-27' texto2='Abre às 11:00' cor={colors.cinzaClaro} />
         <AreaAnuncio titulo='Coração sertanejo' texto1='R. Ari Barroso, 1-27' texto2='Abre às 11:00' cor={colors.cinzaEscuro} />
         <AreaAnuncio titulo='Coração sertanejo' texto1='R. Ari Barroso, 1-27' texto2='Abre às 11:00' cor={colors.cinzaClaro} />
         <AreaAnuncio titulo='Coração sertanejo' texto1='R. Ari Barroso, 1-27' texto2='Abre às 11:00' cor={colors.cinzaEscuro} />
         <AreaAnuncio titulo='Coração sertanejo' texto1='R. Ari Barroso, 1-27' texto2='Abre às 11:00' cor={colors.cinzaClaro} />
+      </ScrollView>
+    );
+  }
+}
+
+export default class TelaDestaquesMapa extends Component {
+  render() {
+    return (
+      <ImageBackground
+        style={{flex:1, alignSelf: 'stretch', height: undefined, width: undefined, left:0}}
+        source={require('../img/mapa_garanhuns.png')}
+      >
+        <Destaques />
       </ImageBackground>
     );
   }
